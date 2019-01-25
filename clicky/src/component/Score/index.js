32aba2ef04c1aac2e0b0c3c8071920256e8e1877
Saba Schock <sababorjsaba@gmail.com>
2019-01-25 15:20:46 -0800

    styling the score board

--- clicky/src/component/Score/index.js
+++ clicky/src/component/Score/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './score.css'
 
 function Score(props){
     return (
-    <ul class="nav">
-        <li className="nav-item">
+    <ul class="nav row scoregame">
+        <li className="nav-item col-md-3">
         <a href="#" className="nav-link">Clicky Game</a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item col-md-3">
         <a href="#" className="nav-link">{props.result}</a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item col-md-3">
         <a href="#" className="nav-link">Score: {props.score} | Top Score: {props.topScore}</a>
         </li>
     </ul>
